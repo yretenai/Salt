65bb46cf552224260c9ae37bcd2641f01122699c
Ada <ada@chronovore.dev>
2024-05-09 19:56:32 +0100

    also look in subbanks

--- Robin/FEV/Models/GuidRef.cs
+++ Robin/FEV/Models/GuidRef.cs
@@ -9,8 +9,11 @@ public record struct GuidRef<T> where T : BaseChunk, IRefOwner {
 
 	public bool TryGetChunk(FEVSoundBank soundBank, [MaybeNullWhen(false)] out T chunk) {
 		foreach (var chunkId in T.ListTypes) {
-			if (soundBank.TryGetChunk<ListChunk>(chunkId, out var list)) {
-				return list.TryGetChunk(Id, out chunk);
+			if ((soundBank.TryGetChunk<ListChunk>(chunkId, out var list) && list.TryGetChunk(Id, out chunk)) ||
+			    (soundBank.Assets?.TryGetChunk<ListChunk>(chunkId, out list) == true && list.TryGetChunk(Id, out chunk)) ||
+			    (soundBank.Strings?.TryGetChunk<ListChunk>(chunkId, out list) == true && list.TryGetChunk(Id, out chunk)) ||
+			    (soundBank.Owner?.TryGetChunk<ListChunk>(chunkId, out list) == true && list.TryGetChunk(Id, out chunk))) {
+				return true;
 			}
 		}
 
